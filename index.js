024fa8e88fa00d19154d447a7ec636fb525d613e
swapnil2me <swapnilmore@iisc.ac.in>
2019-11-10 12:27:07 +0530

    improved presentation, added line thickness and  color controll

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function InitThis() {
 
     $('.myCanvas').mousedown(function (e) {
         mousePressed = true;
-        //console.log($(this)[0].id);
         Draw(ctxM[$(this)[0].id-1], e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
     });
 
@@ -35,8 +34,8 @@ function InitThis() {
 function Draw(ctx,x, y, isDown) {
     if (isDown) {
         ctx.beginPath();
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = "5";
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = $('#selWidth').val();
         ctx.lineJoin = "round";
         ctx.moveTo(lastX, lastY);
         ctx.lineTo(x, y);
@@ -47,49 +46,46 @@ function Draw(ctx,x, y, isDown) {
 }
 
 function clearArea() {
-    // Use the identity matrix while clearing the canvas
-    //ctxA = document.getElementsByClassName('myCanvas')
     for (i = 0, len = ctxA.length, text = ""; i < len; i++) {
       ctx = ctxA[i].getContext("2d");
       ctx.setTransform(1, 0, 0, 1, 0, 0);
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     };
+    document.getElementById('console').innerText =``
+    //delete classifier.addExample
 }
 
-function clearAreaTest() {
-    // Use the identity matrix while clearing the canvas
-    //ctxA = document.getElementsByClassName('myCanvas')
-
+  function clearAreaTest() {
       ctx = ctxA[3].getContext("2d");
       ctx.setTransform(1, 0, 0, 1, 0, 0);
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
+      document.getElementById('console').innerText =``
 }
 
 async function addClasses() {
 
-    // Generate the image data
-
-    console.log('Loading mobilenet..');
-    // Load the model.
+    document.getElementById('console').innerText =`Loading MobileNet...`;
     net = await mobilenet.load();
-    console.log('Successfully loaded model');
-
+    document.getElementById('console').innerText =`MobileNet loaded`;
     for (i = 0, len = ctxA.length, text = ""; i < len-1; i++) {
       ctx = ctxA[i];
       const activation = net.infer(ctx,'conv_preds');
-      classifier.addExample(activation, i);
+      for (var j = 0; j < 3; j++) {
+        classifier.addExample(activation, i);
+        console.log(j);
+      };
     };
+    document.getElementById('console').innerText =`Added New classes`
 };
 async function predictClass() {
-    console.log('Loading mobilenet..');
-    // Load the model.
+    document.getElementById('console').innerText =`Loading MobileNet...`
     net = await mobilenet.load();
-    console.log('Successfully loaded model');
+    document.getElementById('console').innerText =`MobileNet loaded`;
     img = ctxA[3];
     const activation = net.infer(img, 'conv_preds');
     const result = await classifier.predictClass(activation);
     const classes = ['A','B','C'];
+    console.log(result);
     document.getElementById('console').innerText = `
       prediction: ${classes[result.label]}\n
       probability: ${result.confidences[result.label]}
